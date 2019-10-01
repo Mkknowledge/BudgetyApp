f476579fdc38b7226215ac8493f1076a1dc3a916
Mkknowledge <kmayur2124@gmail.com>
2019-10-01 17:01:37 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+ 
+	
+	//BUDGET CONTROLLER
+
+
+ var budgetController = (function() {
+
+ 	//Some Code
+
+ })();
+
+
+
+ 	//UI CONTROLLER
+
+ var UIController = (function() {
+	
+ 	//Some Code
+
+ })();
+
+
+
+ 	//GLOBAL APP CONTROLLERw
+
+ var controller = (function(budgetCtrl, UICtrl) {
+
+ 	document.querySelector('.add__btn').addEventListener('click', function() {
+ 		
+ 			//1. Get the field input data
+
+ 			//2. Add the item to the budget Controller
+
+ 			//3. Add the item to the Ui
+
+ 			//4. Calculate the budget 
+
+ 			//5. Display the budget on the UI
+ 			
+ 	});
+
+
+ })(budgetController, UIController);
